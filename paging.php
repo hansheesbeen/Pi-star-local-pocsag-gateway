c6c5742f61edee8bca10757389ab1b01eb83d17d
hansheesbeen <hans.heesbeen@gmail.com>
2023-01-07 14:51:18 +0100

    Create paging.php
    script

---
+++ paging.php
@@ -0,0 +1,20 @@
+<?php
+#Change secret_key value to a secure combination of characters
+#
+#Do never use this script on public available pi-star instances.
+#There is NO input sanitizing in this script.
+
+$secret_key = "S3cr3t!";
+$ric 		= $_REQUEST['ric'];
+$msg 		= $_REQUEST['msg'];
+$key 		= $_REQUEST['key'];
+$snd 		= 'sudo RemoteCommand 7643 page'.$ric.' "'.$msg.'"';
+	
+if($secret_key != $key){
+	exit();
+}
+else{
+	exec($snd);
+	exit();
+}
+?>
